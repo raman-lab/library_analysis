e9e6cbbc8db242a35d4cb2f532d6dcc474f45f1f
nwhoppe <nwhoppe@wisc.edu>
2018-08-08 06:50:56 -0500

    initial commit of scripts for csv files from Sony sorter

--- flow_histogram.py
+++ flow_histogram.py
@@ -5,9 +5,101 @@
 # created: 7/2/18
 
 import argparse
+import collections
+import numpy as np
 import pandas as pd
+from color_palettes import palette
+
+
+def plot_flow_hist(list_data_dict, medians_df, minimum, maximum, name, n_bins=100):
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+
+    color_list = palette[len(list_data_dict)]
+    almost_black = '#262626'
+    almost_gray = '#808080'
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    i = 0
+    for file_name, data_list in list_data_dict.items():
+        bins = np.logspace(np.log10(min(data_list)), np.log10(max(data_list)), n_bins)
+        ax1.hist(data_list, bins, color=color_list[i], alpha=0.1)
+        ax1.hist(data_list, bins, color=color_list[i], alpha=0.8, histtype='step')
+        ax1.axvline(np.median(data_list), c=color_list[i])
+        if not medians_df.empty:
+            ax1.axvline(medians_df.loc[file_name]['gate_median'], c=color_list[i], linestyle='dashed')
+        i += 1
+
+    ax1.set_xscale('log')
+
+    if minimum:
+        ax1.set_xlim(left=minimum)
+    if maximum:
+        ax1.set_xlim(right=maximum)
+    spines_to_remove = ['top', 'right']
+    for spine in spines_to_remove:
+        ax1.spines[spine].set_visible(False)
+    ax1.xaxis.set_ticks_position('none')
+    ax1.yaxis.set_ticks_position('none')
+    spines_to_keep = ['bottom', 'left']
+    for spine in spines_to_keep:
+        ax1.spines[spine].set_linewidth(0.5)
+        ax1.spines[spine].set_color(almost_black)
+    ax1.xaxis.label.set_color(almost_black)
+    ax1.yaxis.label.set_color(almost_black)
+
+    plt.savefig(name, dpi=300)
+    plt.close()
+
+
+def medians_from_csv(csv_list):
+    medians = []
+    for stat_csv in csv_list:
+        temp_medians = []
+        stats_df = pd.read_csv(stat_csv, index_col=False)
+        median_list = stats_df[' Median'].tolist()
+        for numeric_string in median_list:
+            if not numeric_string.isspace():
+                numeric_string.strip()
+                temp_medians.append(float(numeric_string))
+        temp_medians.pop(0)
+        medians.extend(temp_medians)
+    return medians
+
+
+def main(input_csvs, column, medians_csv, name, minimum, maximum):
+
+    column_data_dict = collections.OrderedDict()
+    for csv in input_csvs:
+        print csv
+        df = pd.read_csv(csv, index_col=False)
+        df = df[df[column] > 0]
+        col_list = df[column].tolist()
+        column_data_dict[csv] = col_list
+
+    medians_df = pd.DataFrame()
+    if medians_csv:
+        medians_df = pd.read_csv(medians_csv, index_col=0)
+        assert (len(medians_df) >= len(column_data_dict)), \
+            'Error: each row of median_csv must correspond to an input csv'
+
+    plot_flow_hist(column_data_dict, medians_df, minimum, maximum, name)
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="""""")
+    parser = argparse.ArgumentParser(description="""plot histograms from input csvs. intended for use on csv files 
+    from Sony FACS machine. may be amenable to other csvs""")
+    parser.add_argument('-c', '--column', default='EGFP-A-Compensated',
+                        help='header of column containing data to be plotted')
+    parser.add_argument('-m', '--medians_csv',
+                        help='csv containing gate-medians for each input csv. first column is input csv name. second '
+                             'column is median for the gate. intended to compare gate-medians to reflow-medians')
+    parser.add_argument('-n', '--name', default='flow_histogram.png', help='name of output png')
+    parser.add_argument('-min', '--minimum', type=float, help='minimum x axis value')
+    parser.add_argument('-max', '--maximum', type=float, help='maximum x axis value')
     required = parser.add_argument_group('required')
+    required.add_argument('-i', '--input_csvs', nargs='*', required=True, help='input csv files')
+
     args = parser.parse_args()
+    main(args.input_csvs, args.column, args.medians_csv, args.name, args.minimum, args.maximum)
--- flow_variance_analysis.py
+++ flow_variance_analysis.py
@@ -5,7 +5,80 @@
 # created: 7/5/18
 
 import argparse
+import collections
+import itertools
+import numpy as np
 import pandas as pd
+from scipy import stats
+import sys
+
+
+def flow_variance_analysis(input_csvs, column, significance):
+    column_data_dict = collections.OrderedDict()
+    variances = []
+    max_p = 0
+    for csv in input_csvs:
+        df = pd.read_csv(csv, index_col=False)
+        df = df[df[column] > 0]
+        col_list = df[column].tolist()
+
+        # natural log transformation
+        log_transformed_list = np.log(col_list)
+        variances.append(np.var(log_transformed_list))
+
+        # test normality of data
+        skew_kurtosis_stat, p_norm = stats.normaltest(log_transformed_list)
+        if p_norm > max_p:
+            max_p = p_norm
+        sys.stdout.write('Normality test for {0} gives p = {1}\n'.format(csv, p_norm))
+        column_data_dict[csv] = log_transformed_list
+
+    variance_ratio = max(variances) / min(variances)
+    sys.stdout.write('\nRatio of max and min variance: ')
+    sys.stdout.write('{0} / {1} = {0}\n\n'.format(max(variances), min(variances), variance_ratio))
+    kruskal_wallis = False
+
+    if max_p > significance and variance_ratio > 3:
+        sys.stdout.write('Normality and variance assumption failed. Kruskal-Wallis will be run\n')
+        kruskal_wallis = True
+        post_hoc = 'Wilcoxon rank sum'
+    elif max_p > significance and variance_ratio <= 3:
+        sys.stdout.write('Normality assumption failed. Kruskal-Wallis will be run\n')
+        kruskal_wallis = True
+        post_hoc = 'Wilcoxon rank sum'
+    elif max_p < significance and variance_ratio > 3:
+        sys.stdout.write('Variance assumption failed. Kruskal-Wallis will be run.\n')
+        kruskal_wallis = True
+        post_hoc = 't-test with unequal variance'
+    else:
+        sys.stdout.write('Normality and variance assumption satisfied. ANOVA will be run\n')
+        post_hoc = 't-test with equal variance'
+    if kruskal_wallis:
+        test_stat, p_test = stats.kruskal(*column_data_dict.values())
+    else:
+        test_stat, p_test = stats.f_oneway(*column_data_dict.values())
+
+    sys.stdout.write('\nStatistical test gives p = {0}\n'.format(p_test))
+    if p_test <= significance:
+        sys.stdout.write('Significant difference between samples\n\n')
+        sys.stdout.write('Post hoc tests: {0}\n'.format(post_hoc))
+
+        for file1, file2 in itertools.combinations(column_data_dict.keys(), 2):
+            if post_hoc == 't-test with equal variance':
+                test_stat, p_post_hoc = stats.ttest_ind(column_data_dict[file1], column_data_dict[file2],
+                                                        equal_var=True)
+            elif post_hoc == 't-test with unequal variance':
+                test_stat, p_post_hoc = stats.ttest_ind(column_data_dict[file1], column_data_dict[file2],
+                                                        equal_var=False)
+            else:
+                test_stat, p_post_hoc = stats.ranksums(column_data_dict[file1], column_data_dict[file2])
+
+            if p_post_hoc <= significance:
+                sys.stdout.write('Significant difference between {0} and {1} (p = {2})\n'.format(
+                    file1, file2, p_post_hoc))
+    else:
+        sys.stdout.write('NO significant difference between samples\n')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="""script to run an anova on input csv files. csv files should have 
@@ -19,6 +92,10 @@ if __name__ == '__main__':
     if assumptions are satisfied, pairwise t-tests will be done.
     """)
     required = parser.add_argument_group('required')
+    parser.add_argument('-c', '--column', default='EGFP-A-Compensated',
+                        help='header of column containing data to be analyzed')
+    parser.add_argument('-s', '--significance', default=0.05, type=float,
+                        help='max p value to reject null and conclude there is a significant difference')
     required.add_argument('-i', '--input_csvs', nargs='*')
     args = parser.parse_args()
-    
+    flow_variance_analysis(args.input_csvs, args.column, args.significance)
